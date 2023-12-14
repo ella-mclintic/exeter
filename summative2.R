da330fca890035a1f8652d7dae79cb56f69b3382
ella-mclintic <151389145+ella-mclintic@users.noreply.github.com>
2023-12-14 09:43:38 +0000

    Add files via upload

--- summative2.R
+++ summative2.R
@@ -18,13 +18,30 @@ trippub <- read_and_subset("trippub.csv", c('HOUSEID', 'TRPMILES', 'R_AGE'))
 ds1 <- merge(hhpub,trippub, by="HOUSEID",all.x=T)
 
 #drop other days of week
-ds2<-[ds1(ds1$TRAVDAY==1),]
+ds2 <- ds1 %>% 
+  filter(TRAVDAY == 1)
 
 #filtering age
-ds3<-ds2[ds2$R_AGE >= 18, ]
+ds3 <- ds2 %>%
+  filter(R_AGE >= 18)
 
 #omit NA values
 ds4 <- na.omit(ds3)
+
+
+
+# Average distance travelled by household
+average_trip_distance <- ds4 %>%
+  group_by(HOUSEID) %>%               # Group the data by household
+  summarize(
+    total_miles = sum(TRPMILES),    # Sum the trip miles for each group
+    num_journeys = n(),                # Count the number of journeys for each group
+    avg_distance = total_miles / num_journeys  # Calculate the average distance
+  )
+
+#join avg distance to ds
+ds5 <- ds4 %>%
+  left_join(average_trip_distance, by = "HOUSEID")
 
 ##Using loops
 #new df results
@@ -32,12 +49,12 @@ results <- data.frame(State = character(), Correlation = numeric(), stringsAsFac
 
 #
 # Loop through each state
-for (state in unique(ds4$HHSTATE)) {
+for (state in unique(ds5$HHSTATE)) {
   # Subset data for the state
-  state_data <- subset(ds4, HHSTATE == state)
+  state_data <- subset(ds5, HHSTATE == state)
   
   # Compute correlation
-  corr <- cor(state_data$HHSIZE, state_data$TRPMILES)
+  corr <- cor(state_data$HHSIZE, state_data$avg_distance)
   
   # Store the result
   results <- rbind(results, data.frame(State = state, Correlation = corr))
@@ -59,7 +76,7 @@ correlation_plot1 <- ggplot(results, aes(x = State, y = Correlation)) +
 correlation_plot1
 
 # Save the plot
-ggsave("correlationplot1.png", plot = correlation_plot1, width = 10, height = 6, units = "in")
+ggsave("correlationplot1new.png", plot = correlation_plot1, width = 10, height = 6, units = "in")
 
 
 ##dplyr method
@@ -67,9 +84,9 @@ ggsave("correlationplot1.png", plot = correlation_plot1, width = 10, height = 6,
 library(dplyr)
 
 # Compute correlation coefficients
-correlation_ds <- ds4 %>%
+correlation_ds <- ds5 %>%
   group_by(HHSTATE) %>%
-  summarize(Correlation = cor(HHSIZE, TRPMILES, use = "complete.obs"))
+  summarize(Correlation = cor(HHSIZE, avg_distance, use = "complete.obs"))
   
 
 # Plotting the results
@@ -85,4 +102,4 @@ correlation_plot2 <- ggplot(correlation_ds, aes(x = HHSTATE, y = Correlation)) +
 correlation_plot2
 
 # Save the plot
-ggsave("correlationplot2.png", plot = correlation_plot2, width = 10, height = 6, units = "in")
+ggsave("correlationplot2new.png", plot = correlation_plot2, width = 10, height = 6, units = "in")
